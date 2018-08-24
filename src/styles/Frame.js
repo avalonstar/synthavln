d16ef0deae7ac9d4bb05319254fec2adc9773138
Bryan Veloso <bryan@avalonstar.com>
2018-08-24 00:22:06 -0700

    Port over Frame.js styles from the existing project.

---
+++ src/styles/Frame.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { rgba } from 'polished';
+
+const Frame = styled.div`
+  position: absolute;
+  overflow: hidden;
+  width: 1920px;
+  height: 1080px;
+`;
+
+const Container = styled.div``;
+
+export default {
+  Frame,
+  Container
+};
