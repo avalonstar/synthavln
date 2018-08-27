f7fb1fef428fe5a20506d6c675ba78db42b03889
Bryan Veloso <bryan@avalonstar.com>
2018-08-27 14:40:16 -0700

    Adding shadows and frame dimensions to foundation.

--- src/styles/foundation.js
+++ src/styles/foundation.js
@@ -1,13 +1,40 @@
 import { injectGlobal } from 'styled-components';
-import { normalize } from 'polished';
+import { fontFace, normalize, rgba } from 'polished';
 
 import colors from './colors';
 
 export const foundation = {
   colors,
   fonts: {
-    montserrat: "'Montserrat', sans-serif"
-  }
+    barlow: 'Barlow, sans-serif',
+    montserrat: 'Montserrat, sans-serif'
+  },
+  frame: {
+    width: '1920px',
+    height: '1080px'
+  },
+  shadows: [
+    `0 1px 3px ${rgba(colors.gray[0], 0.12)}, 0 1px 2px ${rgba(
+      colors.gray[0],
+      0.24
+    )}`,
+    `0 3px 6px ${rgba(colors.gray[0], 0.16)}, 0 3px 6px ${rgba(
+      colors.gray[0],
+      0.23
+    )}`,
+    `0 10px 20px ${rgba(colors.gray[0], 0.19)}, 0 6px 6px ${rgba(
+      colors.gray[0],
+      0.23
+    )}`,
+    `0 14px 28px ${rgba(colors.gray[0], 0.25)}, 0 10px 10px ${rgba(
+      colors.gray[0],
+      0.22
+    )}`,
+    `0 19px 38px ${rgba(colors.gray[0], 0.3)}, 0 15px 12px ${rgba(
+      colors.gray[0],
+      0.22
+    )}`
+  ]
 };
 
 export default () => injectGlobal`
