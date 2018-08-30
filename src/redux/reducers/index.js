665df7cc8787a5c552ea4cba0417f703f100c212
Bryan Veloso <bryan@avalonstar.com>
2018-08-30 01:50:37 -0700

    Really basic reducer for UI things.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
+import ui from './ui';
+
 const rootReducer = combineReducers({
+  ui,
   router
 });
 
