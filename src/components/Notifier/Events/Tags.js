2227b107066e4e01ca6149c85057528435966441
Bryan Veloso <bryan@avalonstar.com>
2018-08-31 15:43:12 -0700

    Aggregating all of the 'tags' into one module.

---
+++ src/components/Notifier/Events/Tags.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import * as utils from './utils';
+
+import styled from 'styled-components';
+import {
+  Gift,
+  CreditCard,
+  Star,
+  Heart,
+  PlusSquare,
+  Square,
+  Zap
+} from 'react-feather';
+
+const svgSize = 16;
+
+export const Cheer = () => (
+  <Tag>
+    <Zap size={svgSize} /> cheer
+  </Tag>
+);
+
+export const MysteryGift = () => (
+  <Tag>
+    <Gift size={svgSize} /> mass subgift
+  </Tag>
+);
+
+export const Resub = () => (
+  <Tag>
+    <Star size={svgSize} /> resub
+  </Tag>
+);
+
+export const SPChange = props => (
+  <Tag>
+    <PlusSquare size={svgSize} /> sp + {utils.getSubValue(props.plan)}
+  </Tag>
+);
+
+export const SPRetain = () => (
+  <Tag>
+    <Square size={svgSize} /> sp retain
+  </Tag>
+);
+
+export const SubGift = () => (
+  <Tag>
+    <Gift size={svgSize} /> subgift
+  </Tag>
+);
+
+export const Subscription = () => (
+  <Tag>
+    <Star size={svgSize} /> sub
+  </Tag>
+);
+
+export const Tip = () => (
+  <Tag>
+    <CreditCard size={svgSize} /> tip
+  </Tag>
+);
+
+export const Tier = props => (
+  <Tag>
+    <Heart size={svgSize} /> {utils.getTier(props.plan)}
+  </Tag>
+);
+
+const Tag = styled.div`
+  display: inline-flex;
+  align-items: center;
+  margin-right: 12px;
+
+  /* box-shadow: inset 0 0 0 1px ${props => props.theme.colors.gray[20]}; */
+  color: ${props => props.theme.colors.gray[20]};
+  border-radius: 2px;
+  text-transform: uppercase;
+
+  svg {
+    padding-right: 4px;
+  }
+`;
