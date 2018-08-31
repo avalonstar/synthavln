35715f5a3814388f05b73ade98b8185f5cb6b112
Bryan Veloso <bryan@avalonstar.com>
2018-08-30 20:37:48 -0700

    Firebase stuff.

--- src/scenes/Root.js
+++ src/scenes/Root.js
@@ -1,3 +1,6 @@
+import firebase from '@firebase/app';
+import '@firebase/firestore';
+import { FirestoreProvider, Firestore } from 'react-firestore';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
@@ -10,6 +13,18 @@ import Activity from 'scenes/Activity';
 import { foundation } from 'styles/foundation';
 import { history } from 'store';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_URI
+} = process.env;
+
+firebase.initializeApp({
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: REACT_APP_FIREBASE_URI
+});
+
 const Main = () => (
   <Switch>
     <Route exact path="/scenes/activity" component={Activity} />
@@ -18,13 +33,15 @@ const Main = () => (
 );
 
 const Root = props => (
-  <Provider store={props.store}>
-    <ConnectedRouter history={history}>
-      <ThemeProvider theme={foundation}>
-        <Main />
-      </ThemeProvider>
-    </ConnectedRouter>
-  </Provider>
+  <FirestoreProvider firebase={firebase}>
+    <Provider store={props.store}>
+      <ConnectedRouter history={history}>
+        <ThemeProvider theme={foundation}>
+          <Main />
+        </ThemeProvider>
+      </ConnectedRouter>
+    </Provider>
+  </FirestoreProvider>
 );
 
 export default Root;
