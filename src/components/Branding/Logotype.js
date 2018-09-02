cdd547eab35cb41b7f45f4e4ecf058fa18446143
Bryan Veloso <bryan@avalonstar.com>
2018-09-02 00:03:49 -0700

    Having some fun with transitions... fun... yes.

--- src/components/Branding/Logotype.js
+++ src/components/Branding/Logotype.js
@@ -2,16 +2,41 @@ import React from 'react';
 
 import Indicator from './Indicator';
 
+import { keyframes, easing } from 'popmotion';
+import SplitText from 'react-pose-text';
 import styled from 'styled-components';
+import colors from 'styles/colors';
+
 import logo from './logo.png';
 
 const Logotype = props => (
   <Wrapper>
     <Avocado src={logo} />
-    Avalonstar
-    <Period>.</Period>
+    <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+      Avalonstar.
+    </SplitText>
   </Wrapper>
 );
+
+const charPoses = {
+  exit: {
+    color: colors.gray[3],
+    opacity: 0
+  },
+  enter: {
+    color: colors.white,
+    opacity: 1,
+    delay: ({ charIndex }) => charIndex * 25,
+    transition: {
+      color: ({ from, to }) =>
+        keyframes({
+          values: [from, '#ff3852', '#f437ff', '#5eccff', to],
+          times: [0, 0.5, 0.75, 0.85, 1],
+          duration: 500
+        })
+    }
+  }
+};
 
 const Wrapper = styled.div`
   display: flex;
