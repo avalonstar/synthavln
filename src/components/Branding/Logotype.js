22ede000be6153fe28f460a8dd1cf536fb63ddf9
Bryan Veloso <bryan@avalonstar.com>
2018-09-03 10:30:03 -0700

    Get a logotype animation going.

--- src/components/Branding/Logotype.js
+++ src/components/Branding/Logotype.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import posed from 'react-pose';
+import SplitText from 'react-pose-text';
+import { keyframes, easing } from 'popmotion';
 
 import Indicator from './Indicator';
 
-import { keyframes, easing } from 'popmotion';
-import SplitText from 'react-pose-text';
 import styled from 'styled-components';
 import colors from 'styles/colors';
 
@@ -11,32 +12,63 @@ import logo from './logo.png';
 
 const Logotype = props => (
   <Wrapper>
-    <Avocado src={logo} />
-    <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
-      Avalonstar.
+    <Avocado
+      src={logo}
+      initialPose="exit"
+      pose={props.isVisible ? 'enter' : 'exit'}
+    />
+    <SplitText
+      charPoses={charPoses}
+      initialPose="exit"
+      pose={props.isVisible ? 'enter' : 'exit'}
+    >
+      Avalonstar
     </SplitText>
+    <Period initialPose="exit" pose={props.isVisible ? 'enter' : 'exit'}>
+      .
+    </Period>
   </Wrapper>
 );
 
+const animationDelay = 300;
+
+const avocadoPoses = {
+  exit: { x: '-200%' },
+  enter: {
+    x: '0%',
+    delay: animationDelay,
+    transition: { type: 'spring', damping: 12 }
+  }
+};
+
+const periodPoses = {
+  exit: { opacity: 0, transform: 'rotate(-180deg) scale(3)' },
+  enter: {
+    opacity: 1,
+    transform: 'rotate(0deg) scale(1)',
+    delay: animationDelay,
+    transition: { type: 'spring' }
+  }
+};
+
 const charPoses = {
   exit: {
-    color: colors.gray[3],
-    opacity: 0
+    color: colors.gray[2]
   },
   enter: {
     color: colors.white,
-    opacity: 1,
-    delay: ({ charIndex }) => charIndex * 25,
-    transition: {
-      color: ({ from, to }) =>
-        keyframes({
-          values: [from, '#ff3852', '#f437ff', '#5eccff', to],
-          times: [0, 0.5, 0.75, 0.85, 1],
-          duration: 500
-        })
-    }
+    delay: ({ charIndex }) => animationDelay + charIndex * 20,
+    transition: { color: ({ from, to }) => charKeyframes(from, to) }
   }
 };
+
+const charKeyframes = (from, to) =>
+  keyframes({
+    values: [from, '#00EAC9', '#02FA7B', '#FFF583', to],
+    times: [0, 0.5, 0.75, 0.85, 1],
+    duration: 600,
+    easing: easing.anticipate
+  });
 
 const Wrapper = styled.div`
   display: flex;
@@ -50,13 +82,13 @@ const Wrapper = styled.div`
   text-transform: uppercase;
 `;
 
-const Avocado = styled.img`
+const Avocado = styled(posed.img(avocadoPoses))`
   margin-right: 8px;
   width: 28px;
   height: 28px;
 `;
 
-const Period = styled.span`
+const Period = styled(posed.span(periodPoses))`
   color: ${props => props.theme.colors.green[0]};
 `;
 
