1c1e1e29690503f6b65e63338c453f45d4334ec3
Bryan Veloso <bryan@avalonstar.com>
2018-09-25 01:03:55 -0700

    Add emote progress to the summary.

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import posed from 'react-pose';
 import { easing } from 'popmotion';
 
+import * as Providers from 'providers';
+
 import styled from 'styled-components';
 import { Heart } from 'react-feather';
 
@@ -14,15 +16,36 @@ const defaultProps = {
   isVisible: false
 };
 
+const statisticPropTypes = {
+  score: PropTypes.string.isRequired,
+  goal: PropTypes.string.isRequired
+};
+
+const Statistic = ({ score, goal }) => {
+  const { minimum_score: next, emoticon_limit: slot } = goal;
+  return (
+    <Stat>
+      <Heart color="#eaf56b" size={18} />
+      {score}/{next}
+    </Stat>
+  );
+};
+
 const Summary = ({ isVisible }) => (
   <Wrapper initialPose="exit" pose={isVisible ? 'enter' : 'exit'}>
-    <Heart size={18} />
-    <Stat>
-      <strong>5 new</strong> crusaders!
-    </Stat>
+    <Title>Next Emote</Title>
+    <Providers.Broadcaster>
+      {({ subscriptions }) => (
+        <Statistic
+          score={subscriptions.score}
+          goal={subscriptions.next_level}
+        />
+      )}
+    </Providers.Broadcaster>
   </Wrapper>
 );
 
+Statistic.propTypes = statisticPropTypes;
 Summary.propTypes = propTypes;
 Summary.defaultProps = defaultProps;
 
@@ -39,7 +62,6 @@ const Wrapper = styled(posed.div(summaryPoses))`
   display: flex;
   align-items: center;
   margin-left: 12px;
-  padding: 8px 16px;
 
   box-shadow: inset 0 0 0 1px ${props => props.theme.colors.gray[6]};
   border-radius: 36px;
@@ -49,15 +71,27 @@ const Wrapper = styled(posed.div(summaryPoses))`
   transition: all 250ms ${props => props.theme.easing};
   opacity: 0;
   white-space: nowrap;
+`;
 
-  strong {
-    font-weight: 800;
-  }
+const Title = styled.div`
+  padding: 8px 12px 8px 16px;
+  color: ${props => props.theme.colors.gray[18]};
+  font-family: ${props => props.theme.fonts.din};
+  font-weight: 600;
 `;
 
 const Stat = styled.div`
-  display: none;
-  margin-left: 4px;
+  display: flex;
+  align-items: center;
+  margin-left: 0;
+  padding: 8px 16px 8px 12px;
+
+  box-shadow: inset 1px 0 0 0 ${props => props.theme.colors.gray[6]};
+  font-weight: 800;
+
+  svg {
+    margin-right: 4px;
+  }
 `;
 
 export default Summary;
