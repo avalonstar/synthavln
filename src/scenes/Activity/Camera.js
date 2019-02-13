f803e3fd757ead1fa743b3609cb9e5cd6b89acfd
Bryan Veloso <bryan@avalonstar.com>
2019-02-12 17:42:46 -0800

    Remove the frame on Camera to make it more easily positionable.

--- src/scenes/Activity/Camera.js
+++ src/scenes/Activity/Camera.js
@@ -3,21 +3,13 @@ import React from 'react';
 import { Camera } from 'components';
 
 import styled from 'styled-components';
-import { Frame } from 'styles';
 
-const Scene = () => (
-  <Frame.Wrapper>
-    <StyledCamera />
-  </Frame.Wrapper>
-);
+const Scene = () => <StyledCamera />;
 
 const StyledCamera = styled(Camera)`
-  grid-row: 25;
-  grid-column: 2;
-  justify-self: end;
-  align-self: end;
-  margin-right: 36px;
-  width: 422px;
+  margin: 36px;
+  width: 448px;
+  /* width: 422px; */
   z-index: 1000;
 `;
 
