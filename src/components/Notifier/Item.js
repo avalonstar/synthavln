0bc429bfae2a434f37e6853ef3a32213a41d9968
Bryan Veloso <bryan@avalonstar.com>
2019-02-14 10:20:22 -0800

    Convert our first component to use hooks!

--- src/components/Notifier/Item.js
+++ src/components/Notifier/Item.js
@@ -1,8 +1,8 @@
-import _ from 'lodash';
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Sound from 'react-sound';
 import posed from 'react-pose';
+import isEmpty from 'lodash/isEmpty';
 
 import styled from 'styled-components';
 
@@ -14,23 +14,9 @@ import {
   SubGiftEvent,
   RaidEvent,
   ResubEvent,
-  TipEvent
+  TipEvent,
 } from './Events';
 import { getSongFile } from './utils';
-
-const propTypes = {
-  notification: PropTypes.shape({
-    event: PropTypes.string
-  }),
-  className: PropTypes.string.isRequired,
-  onComplete: PropTypes.func.isRequired,
-  soundOnly: PropTypes.bool
-};
-
-const defaultProps = {
-  notification: {},
-  soundOnly: false
-};
 
 const getType = data => ({
   cheer: CheerEvent({ ...data }),
@@ -40,74 +26,70 @@ const getType = data => ({
   subgift: SubGiftEvent({ ...data }),
   raid: RaidEvent({ ...data }),
   resub: ResubEvent({ ...data }),
-  tip: TipEvent({ ...data })
+  tip: TipEvent({ ...data }),
 });
 
-class Item extends PureComponent {
-  state = {
-    isVisible: false,
-    playStatus: Sound.status.STOPPED,
-    volume: 20
-  };
+function Item({ className, notification, soundOnly, onComplete }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const [playStatus, setPlayStatus] = useState('STOPPED');
+  const [volume, setVolume] = useState(20);
 
-  componentWillReceiveProps(nextProps) {
-    const { notification } = this.props;
-    if (nextProps.notification && nextProps.notification !== notification) {
-      this.timer = setTimeout(() => {
-        this.setState({ isVisible: true });
-        setTimeout(
-          () => this.setState({ playStatus: Sound.status.PLAYING }),
-          600
-        );
-      });
+  useEffect(() => {
+    if (!soundOnly) {
+      setVolume(0);
     }
+  }, []);
+
+  useEffect(
+    () => {
+      setIsVisible(true);
+      setTimeout(() => setPlayStatus('PLAYING'), 600);
+    },
+    [notification]
+  );
+
+  function handleFinishedPlaying() {
+    setPlayStatus('STOPPED');
+    setIsVisible(false);
+    return setTimeout(() => onComplete(), 500);
   }
 
-  handleRest = () => {
-    const { onComplete } = this.props;
-    const { isVisible } = this.state;
-    if (!isVisible) {
-      setTimeout(() => onComplete(), 500);
-    }
-  };
+  const baseURL = 'http://synthform.s3.amazonaws.com/audio/avalonstar/';
 
-  handleSongFinishedPlaying = () => {
-    this.setState({
-      isVisible: false,
-      playStatus: Sound.status.STOPPED
-    });
-    clearTimeout(this.timer);
-    this.handleRest();
-  };
-
-  render() {
-    const { className, notification, soundOnly } = this.props;
-    const { isVisible, playStatus, volume } = this.state;
-    const baseURL = 'http://synthform.s3.amazonaws.com/audio/avalonstar/';
-    return _.isEmpty(notification) ? null : (
-      <Wrapper
-        className={className}
-        initialPose="hide"
-        pose={isVisible ? 'show' : 'hide'}
-      >
-        {!soundOnly && getType(notification)[notification.event]}
-        <Sound
-          url={`${baseURL}${getSongFile(notification)}.ogg`}
-          playStatus={playStatus}
-          onFinishedPlaying={this.handleSongFinishedPlaying}
-          volume={soundOnly ? volume : 0}
-        />
-      </Wrapper>
-    );
-  }
+  return isEmpty(notification) ? null : (
+    <Wrapper
+      className={className}
+      initialPose="hide"
+      pose={isVisible ? 'show' : 'hide'}
+    >
+      {!soundOnly && getType(notification)[notification.event]}
+      <Sound
+        url={`${baseURL}${getSongFile(notification)}.ogg`}
+        playStatus={playStatus}
+        onFinishedPlaying={handleFinishedPlaying}
+        volume={volume}
+      />
+    </Wrapper>
+  );
 }
 
-Item.propTypes = propTypes;
-Item.defaultProps = defaultProps;
+Item.propTypes = {
+  notification: PropTypes.shape({
+    event: PropTypes.string,
+  }),
+  className: PropTypes.string.isRequired,
+  onComplete: PropTypes.func.isRequired,
+  soundOnly: PropTypes.bool,
+};
+
+Item.defaultProps = {
+  notification: {},
+  soundOnly: false,
+};
 
 const wrapperProps = {
   hide: { x: '-100%' },
-  show: { x: '0%', transition: { ease: 'anticipate', duration: 1000 } }
+  show: { x: '0%', transition: { ease: 'anticipate', duration: 1000 } },
 };
 
 const Wrapper = styled(posed.div(wrapperProps))`
