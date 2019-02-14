83d312c09dcacba8f23793adf0c1c4ea959c3aa8
Bryan Veloso <bryan@avalonstar.com>
2019-02-14 10:31:16 -0800

    Fix the devtools warning.

--- src/store.js
+++ src/store.js
@@ -11,9 +11,9 @@ const configureStore = () => {
   const middleware = [];
 
   if (process.env.NODE_ENV === 'development') {
-    const { devToolsExtension } = window;
-    if (typeof devToolsExtension === 'function') {
-      enhancers.push(devToolsExtension());
+    const { __REDUX_DEVTOOLS_EXTENSION__ } = window;
+    if (typeof __REDUX_DEVTOOLS_EXTENSION__ === 'function') {
+      enhancers.push(__REDUX_DEVTOOLS_EXTENSION__());
     }
 
     const { logger } = require(`redux-logger`); // eslint-disable-line
