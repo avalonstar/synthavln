9969128faba5c166610dc12b58ae3dcabf025a18
Bryan Veloso <bryan@avalonstar.com>
2019-05-13 16:46:33 -0700

    Add Dave's icons as components.

---
+++ src/components/Icons/Notifier/bits.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function Bits() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0)">
+        <path
+          d="M6.32607 0.636996C6.28694 0.588468 6.23743 0.54932 6.18119 0.522424C6.12495 0.495528 6.06341 0.481567 6.00107 0.481567C5.93873 0.481567 5.87718 0.495528 5.82094 0.522424C5.7647 0.54932 5.7152 0.588468 5.67607 0.636996L1.81557 5.1285C1.7278 5.23894 1.67728 5.37436 1.67126 5.5153C1.66524 5.65624 1.70403 5.79547 1.78207 5.913L5.70757 11.35C5.73756 11.4009 5.78032 11.4431 5.83162 11.4724C5.88292 11.5017 5.94098 11.5171 6.00007 11.5171C6.05915 11.5171 6.11721 11.5017 6.16851 11.4724C6.21981 11.4431 6.26257 11.4009 6.29257 11.35L10.2181 5.911C10.2961 5.79347 10.3349 5.65424 10.3289 5.5133C10.3229 5.37236 10.2723 5.23694 10.1846 5.1265L6.32607 0.636996Z"
+          stroke="#FFDD33"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4.00806 6.25798L5.98956 0.508484L7.99206 6.25798H4.00806Z"
+          stroke="#FFDD33"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.99194 6.25802L10.3289 5.56152"
+          stroke="#FFDD33"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.99206 6.258L5.98956 11.5085L4.00806 6.258H7.99206Z"
+          stroke="#FFDD33"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4.0079 6.25802L1.6709 5.56152"
+          stroke="#FFDD33"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0">
+          <rect width="12" height="12" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default Bits;
---
+++ src/components/Icons/Queue/bits.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Bits() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.6519 1.27399C12.5736 1.17694 12.4746 1.09864 12.3621 1.04485C12.2497 0.991055 12.1266 0.963135 12.0019 0.963135C11.8772 0.963135 11.7541 0.991055 11.6416 1.04485C11.5292 1.09864 11.4302 1.17694 11.3519 1.27399L3.63089 10.257C3.45535 10.4779 3.35432 10.7487 3.34228 11.0306C3.33024 11.3125 3.40782 11.5909 3.56389 11.826L11.4149 22.7C11.4749 22.8018 11.5604 22.8862 11.663 22.9448C11.7656 23.0035 11.8817 23.0343 11.9999 23.0343C12.1181 23.0343 12.2342 23.0035 12.3368 22.9448C12.4394 22.8862 12.5249 22.8018 12.5849 22.7L20.4359 11.822C20.592 11.5869 20.6695 11.3085 20.6575 11.0266C20.6455 10.7447 20.5444 10.4739 20.3689 10.253L12.6519 1.27399Z"
+        stroke="#EFEFEF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.01611 12.516L11.9791 1.01703L15.9841 12.516H8.01611Z"
+        stroke="#EFEFEF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.9839 12.516L20.6579 11.123"
+        stroke="#EFEFEF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.9841 12.516L11.9791 23.017L8.01611 12.516H15.9841Z"
+        stroke="#EFEFEF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.01604 12.516L3.34204 11.123"
+        stroke="#EFEFEF"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default Bits;
