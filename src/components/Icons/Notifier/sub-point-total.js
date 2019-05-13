4576296eca0c0378f86d05bc23abfcd80dda41da
Bryan Veloso <bryan@avalonstar.com>
2019-05-13 16:55:06 -0700

    Fix SVG attributes.

--- src/components/Icons/Notifier/sub-point-total.js
+++ src/components/Icons/Notifier/sub-point-total.js
@@ -9,48 +9,48 @@ function SubPointTotal() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0)">
+      <g clipPath="url(#clip0)">
         <path
           d="M11.252 1.75H0.751953C0.475811 1.75 0.251953 1.97386 0.251953 2.25V9.75C0.251953 10.0261 0.475811 10.25 0.751953 10.25H11.252C11.5281 10.25 11.752 10.0261 11.752 9.75V2.25C11.752 1.97386 11.5281 1.75 11.252 1.75Z"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M2.25195 8.25V7.25"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M3.75195 8.25V5.75"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M5.25195 8.25V6.75"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M9.75195 8.25V4.75"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M8.25195 8.25V6"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M6.75195 8.25V3.75"
           stroke="#FFDD33"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
--- src/components/Icons/Ticker/sub-point-total.js
+++ src/components/Icons/Ticker/sub-point-total.js
@@ -9,48 +9,48 @@ function SubPointTotal() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0)">
+      <g clipPath="url(#clip0)">
         <path
           d="M22.5039 3.5H1.50391C0.951621 3.5 0.503906 3.94772 0.503906 4.5V19.5C0.503906 20.0523 0.951621 20.5 1.50391 20.5H22.5039C23.0562 20.5 23.5039 20.0523 23.5039 19.5V4.5C23.5039 3.94772 23.0562 3.5 22.5039 3.5Z"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M4.50391 16.5V14.5"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M7.50391 16.5V11.5"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.5039 16.5V13.5"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M19.5039 16.5V9.5"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.5039 16.5V12"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M13.5039 16.5V7.5"
           stroke="#B4CBD6"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
