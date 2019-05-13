06c0a29d8e332dee5fee748bc700c3f663bf7963
Bryan Veloso <bryan@avalonstar.com>
2019-05-13 12:03:38 -0700

    Add the new brand color palette.

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,26 @@
 export default {
+  main: {
+    dark: '#241f33',
+    light: '#efefef',
+    avagreen: '#5be058',
+    avapurple: '#6644e8',
+    avablue: '#1cdaf4',
+    avayellow: '#ffdd33'
+  },
+  muted: {
+    green: '#bbf4b0',
+    purple: '#928add',
+    bluegrey: '#6d8591',
+    yellow: '#fff683',
+    lightgreen: '#e7f7e7',
+    lightbluegrey: '#b4cbd6',
+    dark: '#0d0a11',
+    midgrey: '#939393'
+  },
+  gradient: {
+    lighter: '#23292f',
+    darker: '#1a1f23'
+  },
   black: '#000000',
   white: '#ffffff',
   gray: [
