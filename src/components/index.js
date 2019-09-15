b6767f171b79ad1e9845c0f0ac0f6ef00949fb5e
Bryan Veloso <bryan@avalonstar.com>
2019-09-15 11:36:42 -0700

    Feature: Implement the EmoteBucketSystem.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Animated } from './Ava';
 export { Ava, Logo, Logotype } from './Branding';
 export { default as Camera } from './Camera';
 export { default as Debug } from './Debug';
---
+++ src/helpers/EmoteBucketSystem.js
@@ -0,0 +1,98 @@
+// System defaults.
+const EMOTE_THRESHOLD = 100;
+const TIME_THRESHOLD = 1 * 60 * 1000;
+const SYSTEM_COOLDOWN = 5 * 60 * 1000;
+
+export default class EmoteBucketSystem {
+  constructor({
+    emoteThreshold = EMOTE_THRESHOLD,
+    timeThreshold = TIME_THRESHOLD,
+    systemCooldown = SYSTEM_COOLDOWN,
+    trackedEmotes,
+    onThresholdReached
+  }) {
+    if (
+      !trackedEmotes ||
+      !Array.isArray(trackedEmotes) ||
+      !trackedEmotes.length
+    ) {
+      throw new Error('No trackedEmotes array provided');
+    }
+    if (!onThresholdReached || typeof onThresholdReached !== 'function') {
+      throw new Error('No onThresholdReached function provided');
+    }
+
+    this.onThresholdReached = onThresholdReached;
+    this.emoteThreshold = emoteThreshold;
+    this.timeThreshold = timeThreshold;
+    const systemThroughput = this.emoteThreshold / this.timeThreshold;
+    this.depletionRate = (this.emoteThreshold - 1) / systemThroughput;
+    this.systemCooldown = systemCooldown;
+    this.trackedEmotes = trackedEmotes;
+    this.blocked = false;
+    this.initializeBuckets();
+    this.startTimers();
+  }
+
+  initializeBuckets() {
+    this.buckets = {};
+    this.trackedEmotes.forEach(emote => {
+      this.buckets[emote] = 0;
+    });
+  }
+
+  startTimers() {
+    this.leakingTimer = setInterval(() => {
+      if (!this.blocked) {
+        Object.keys(this.buckets).forEach(emote => {
+          if (this.buckets[emote] > 0) {
+            this.buckets[emote] -= 1;
+          }
+        });
+      }
+    }, this.depletionRate);
+  }
+
+  stopTimers() {
+    if (this.leakingTimer) clearInterval(this.leakingTimer);
+  }
+
+  processIncomgingEmote(emote) {
+    if (!this.blocked) {
+      if (this.buckets[emote] !== null && this.buckets[emote] !== undefined) {
+        this.buckets[emote] += 1;
+        if (this.buckets[emote] >= this.emoteThreshold) {
+          this.fireThresholdEvent(emote);
+        }
+      }
+    }
+  }
+
+  fireThresholdEvent(e) {
+    if (this.onThresholdReached) {
+      this.onThresholdReached(e);
+    }
+    this.initiateCooldown();
+  }
+
+  initiateCooldown() {
+    this.blocked = true;
+    this.initializeBuckets();
+    this.cooldownTimer = setTimeout(() => {
+      this.blocked = false;
+    }, this.systemCooldown);
+  }
+
+  reset() {
+    this.stopTimers();
+    if (this.cooldownTimer) clearTimeout(this.cooldownTimer);
+    this.blocked = false;
+    this.initializeBuckets();
+    this.startTimers();
+  }
+
+  destroy() {
+    this.stopTimers();
+    if (this.cooldownTimer) clearTimeout(this.cooldownTimer);
+  }
+}
