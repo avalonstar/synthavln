46819aa3b60a87f443f56026bfec06b4af609a6a
Bryan Veloso <bryan@avalonstar.com>
2019-09-21 12:34:30 -0700

    Fix: Remove unnecessary context providers.

--- src/components/Emotes/Toy.js
+++ src/components/Emotes/Toy.js
@@ -4,8 +4,6 @@ import * as PIXI from 'pixi.js';
 
 import styled from 'styled-components';
 
-import { useMessageContext, useNotificationContext } from 'providers';
-
 import Scene from './Scene';
 
 PIXI.utils.skipHello();
@@ -16,11 +14,7 @@ function Toy() {
   return (
     <Wrapper>
       <Stage width={WIDTH} height={HEIGHT} options={{ transparent: true }}>
-        <useMessageContext.Provider>
-          <useNotificationContext.Provider>
-            <Scene width={WIDTH} height={HEIGHT} />
-          </useNotificationContext.Provider>
-        </useMessageContext.Provider>
+        <Scene width={WIDTH} height={HEIGHT} />
       </Stage>
     </Wrapper>
   );
